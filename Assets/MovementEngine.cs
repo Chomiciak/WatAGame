68d7619bcb2b5ea62e0623f358a6941c8db6db54
Chomiciak <Chomiciak@gmail.com>
2017-01-03 17:46:01 +0100

    Pierwsze zajecia z Unity3D w 2017!

---
+++ Assets/MovementEngine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementEngine : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		Rigidbody2D body = GetComponent<Rigidbody2D> ();
+
+
+		if(Input.GetKeyDown(KeyCode.W))
+			body.velocity = new Vector2 (body.velocity.x, 5);
+		if(Input.GetKeyDown(KeyCode.A))
+			body.velocity = new Vector2 (-5, body.velocity.y);
+		if(Input.GetKeyDown(KeyCode.S))
+			body.velocity = new Vector2 (body.velocity.x, -5);
+		if(Input.GetKeyDown(KeyCode.D))
+			body.velocity = new Vector2 (5, body.velocity.y);
+		
+
+	}
+}
